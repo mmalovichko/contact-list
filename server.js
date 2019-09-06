04d10348c7eb4c2cb549e0f12faf98cd85feb4bf
mmalovichko <malovichko.maksym@gmail.com>
2019-09-06 11:00:21 -0400

    done1

--- server.js
+++ server.js
@@ -5,47 +5,54 @@ const cors = require('cors');
 const compression = require('compression');
 const mongodb = require('mongodb')
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://AndrewBubnov:acnot88_0175A@cluster0-shard-00-00-edszp.mongodb.net:27017,cluster0-shard-00-01-' +
-    'edszp.mongodb.net:27017,cluster0-shard-00-02-edszp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+const url = 'mongodb://localhost:27017';
 const PORT = process.env.PORT || 3000;
 let currentDB;
+let dbConnectionError = false;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(compression());
 
-app.get('', async (req, res) => {
-    const contacts = await currentDB.collection('contacts').find().toArray()
-    res.send(contacts);
+app.get('/api/get', async (req, res) => {
+    if (!dbConnectionError) {
+        try {
+            const contacts = await currentDB.collection('contacts').find().toArray();
+            res.send(JSON.stringify(contacts));
+        } catch (error) {
+            res.status(503).send("Something wrong's happened on server. Please reload the page")
+        }
+    } else res.status(503).send('Can not connect to DB at the time. Please try again later')
 })
 
-app.post('/api', (req, res) => {
+app.post('/api/add', (req, res) => {
     currentDB.collection('contacts').insertOne(req.body, (err, result) => {
         if (err) console.log(err);
-        res.send(req.body);
+        res.send(JSON.stringify({status: 'Ok'}));
     });
 });
 
-app.put('/api', (req, res) => {
+app.put('/api/edit', (req, res) => {
+    const contact = {id: Number(req.body.id)}
     const set = {}
     const newKeys = Object.keys(req.body).filter(item => item.substring(0, 2) !== '$$' && item.substring(0, 1) !== '_')
     newKeys.forEach(item => set[item] = req.body[item])
-    currentDB.collection('contacts').updateOne({_id: mongodb.ObjectID(req.body._id)}, { $set: set }, (err, result) => {
+    currentDB.collection('contacts').updateOne(contact, { $set: set }, (err, res) => {
         if (err) console.log(err);
-        res.send(200)
     })
 });
 
-app.delete('/api/:id', (req, res) => {
-    currentDB.collection('contacts').deleteOne({_id: mongodb.ObjectID(req.params.id)}, (err, result) => {
+app.delete('/api/delete/:id', (req, res) => {
+    const contact = {id: Number(req.params.id)}
+    currentDB.collection('contacts').deleteOne(contact, (err, result) => {
         if (err) console.log(err);
-        res.send(200)
     });
 })
 
 MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
-    if (err) throw Error;
+    if (err) dbConnectionError = true;
     console.log("Connected correctly to DB server");
+    dbConnectionError = false;
     currentDB = db.db("cluster0");
     app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
 
